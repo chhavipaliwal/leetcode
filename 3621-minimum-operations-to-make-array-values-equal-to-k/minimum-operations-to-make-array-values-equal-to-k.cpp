fc104332a6d660d9d1aef1766fed09a0ff65b6dc
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-09 23:05:22 +0530

    Time: 19 ms (11.01%) | Memory: 32.7 MB (5.97%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) 
+    {
+        set<int> s(nums.begin(),nums.end());
+        int small = *min_element(nums.begin(),nums.end());
+        if(small < k)
+            return -1;
+        if(small == k)
+            return s.size()-1;
+        return s.size();
+    }
+};
