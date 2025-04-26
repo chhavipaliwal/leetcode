0f3d7511d14baadc04f3a0480dc826076b105f24
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-26 19:22:07 +0530

    Time: 6 ms (22.38%) | Memory: 93.9 MB (62.97%) - LeetSync

---
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int n = nums.size();
+        long long count =0;
+        int lastMinK=-1, lastMaxK=-1, lastInvalid=-1;
+        for(int i=0; i<n; i++){
+            if(nums[i]<minK || nums[i]>maxK){
+                lastInvalid= i;
+            }
+            if(nums[i]==minK){
+                lastMinK=i;
+            }
+            if(nums[i]==maxK){
+                lastMaxK=i;
+            }
+            int validStart = min(lastMinK, lastMaxK);
+            if(validStart>lastInvalid){
+                count+=(validStart-lastInvalid);
+            }
+
+        }
+        return count;
+    }
+};
